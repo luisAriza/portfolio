860308cc93dea2afd28e0a6114a4e731742d42d0
Luis Ariza <luismanuelariza@hotmail.com>
2022-09-29 17:11:14 -0500

    [UPDATE] Curriculum

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -3,7 +3,7 @@ section.profile-container
 	article.profile
 		h2 Hola, soy Luis Ariza. Gusto en conocerte.
 		p Soy desarrollador web autodidacta, me apasiona darle vida a páginas y aplicaciones web con el objetivo de asegurar el funcionamiento con el navegador, la accesibilidad del usuario y un buen performance. En mis trabajos se destaca una interfaz de usuario intuitiva y agradable a la vista, además de buenas prácticas y documentación del código.
-		a.btn(href="https://drive.google.com/file/d/12ZDu2Qcz9gdsHv6hU5tRiPwC36LD8oTY/view?usp=sharing", target="_blank")
+		a.btn(href="https://drive.google.com/file/d/1j8kKN7XKfaQdXoDDa8TvezBigOlioYyt/view?usp=sharing", target="_blank")
 			| Descargar Curriculum
 			DocumentArrowDownIcon.icon
 </template>
